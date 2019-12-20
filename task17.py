32d161f3d8be547765732e53c7770f10d388dec9
grewy <mgatuiet@gmail.com>
2019-12-21 01:22:47 +0530

    solution for task-17

---
+++ task17.py
@@ -0,0 +1,11 @@
+"""
+ Task 17
+ Write a function that returns the (17*Value) and print the return value of the function. Pass the value 14 to the function and store the return value into a variable 'a'
+ Print the value of 'a'
+"""
+
+def func(val):
+    return 17*val
+
+a = func(14)
+print(a)
