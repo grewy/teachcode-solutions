4c3d0dbd3c1174aa6c5aeb2487201b47bfc888d8
grewy <mgatuiet@gmail.com>
2019-12-21 00:45:57 +0530

    solution for task-15

---
+++ task15.py
@@ -0,0 +1,8 @@
+"""
+Task 15
+ Define a function that calculates area of a square. Pass the value side which is initialized to 20 and print the area by using the function.
+"""
+def square_area(a=20):
+    print(a**2)
+
+square_area()
