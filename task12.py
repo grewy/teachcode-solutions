ee7de0f8d5e98aa58e60be2577ca046bcb7693c1
grewy <mgatuiet@gmail.com>
2019-12-20 07:39:13 +0530

    solution for task-12

---
+++ task12.py
@@ -0,0 +1,17 @@
+"""
+ Task 12
+ Store a string, ' This is Cool!'
+ Your task is to
+ 1. Remove the white spaces at the beginning and the end and print it.
+ 2. Find the length of the string and print it.
+ 3. Print it in lower case.
+ 4. Print it in upper case
+ 5. Replace T with t and print it.
+"""
+
+a= ' This is Cool!'
+print(a.strip())
+print(len(a))
+print(a.lower())
+print(a.upper())
+print(a.replace('T', 't'))
